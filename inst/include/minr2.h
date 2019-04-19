bdfa39fcba4676efb3ba5511c283e640a322515a
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-04-18 17:56:58 -0700

    try removing headers

--- inst/include/minr2.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINR2_H
-#define MINR2_H
-
-#include <RcppArmadillo.h>
-using namespace Rcpp;
-Rcpp::List minR2_c(Rcpp::NumericMatrix x);
-
-#endif
--- inst/include/pca.h
+++
@@ -1,8 +0,0 @@
-#ifndef PCA_H
-#define PCA_H
-
-#include <RcppArmadillo.h>
-using namespace Rcpp;
-List pca_c(arma::mat x);
-
-#endif
