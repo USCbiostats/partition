50fb11edc4517f245634dc38c9a28c361f9845fa
katelynqueen98 <kjqueen@usc.edu>
2023-12-08 10:50:40 -0800

    Update to snake_case

--- R/superPartition.R
+++ R/superPartition.R
@@ -1,10 +1,10 @@
-#' superPartition
+#' super_partition
 #'
-#' @description `superPartition` implements the agglomerative, data reduction method Partition for datasets with large numbers of features by first 'super-partitioning' the data into smaller clusters to Partition.
+#' @description `super_partition` implements the agglomerative, data reduction method Partition for datasets with large numbers of features by first 'super-partitioning' the data into smaller clusters to Partition.
 #'
-#' @param fullData sample by feature data frame or matrix
+#' @param full_data sample by feature data frame or matrix
 #' @param threshold information loss criterion between 0 and 1; default is 0.5
-#' @param clusterSize maximum size of any single cluster; default is 4000
+#' @param cluster_size maximum size of any single cluster; default is 4000
 #' @param threshold the minimum proportion of information explained by a reduced variable; `threshold` sets a boundary for information loss because each reduced variable must explain at least as much as `threshold` as measured by the metric.
 #' @param partitioner a `partitioner`. See the `part_*()` functions and [`as_partitioner()`].
 #' @param tolerance a small tolerance within the threshold; if a reduction is within the threshold plus/minus the tolerance, it will reduce.
@@ -13,7 +13,7 @@
 #' @param .sep a character vector that separates `x` from the number (e.g. "reduced_var_1").
 #' @return Partition object
 #'
-#' @details `superPartition` scales up partition with an approximation, using Genie, a fast, hierarchical clustering algorithm with similar qualities of those to Partition, to first super-partition the data into ceiling(N/c) clusters, where N is the number of features in the full dataset and c is the user-defined maximum cluster size (default value = 4,000). Then, if any cluster from the super-partition has a size greater than c, use Genie again on that cluster until all cluster sizes are less than c. Finally, apply the Partition algorithm to each of the super-partitions.
+#' @details `super_partition` scales up partition with an approximation, using Genie, a fast, hierarchical clustering algorithm with similar qualities of those to Partition, to first super-partition the data into ceiling(N/c) clusters, where N is the number of features in the full dataset and c is the user-defined maximum cluster size (default value = 4,000). Then, if any cluster from the super-partition has a size greater than c, use Genie again on that cluster until all cluster sizes are less than c. Finally, apply the Partition algorithm to each of the super-partitions.
 #'
 #' It may be the case that large super-partitions cannot be easily broken with Genie due to high similarity between features. In this case, we use k-means to break the cluster.
 #'
@@ -23,7 +23,7 @@
 #' df <- simulate_block_data(c(15, 25, 10), lower_corr = .4, upper_corr = .6, n = 100)
 #'
 #' #  don't accept reductions where information < .6
-#' prt <- superPartition(df, threshold = .6, clusterSize = 30)
+#' prt <- super_partition(df, threshold = .6, cluster_size = 30)
 #' prt
 #'
 #' @references
@@ -40,108 +40,106 @@
 #' @export
 #' @import progress
 #' @import genieclust
-superPartition <- function(fullData, threshold = 0.5, clusterSize = 4000, partitioner = part_icc(), tolerance = .0001, niter = NULL, x = "reduced_var", .sep = "_") {
+super_partition <- function(full_data, threshold = 0.5, cluster_size = 4000, partitioner = part_icc(), tolerance = .0001, niter = NULL, x = "reduced_var", .sep = "_") {
 
   # ensure 0 < threshold < 1
   if(0 > threshold | 1 < threshold) stop("Threshold must be between 0 and 1.")
 
   # ensure data frame structure
-  fullData <- as.data.frame(fullData)
+  full_data <- as.data.frame(full_data)
 
   # if < 4,000 features, call regular partition
-  if(ncol(fullData) < clusterSize) {
-    message(paste0("Using regular Partition function since there are < ", clusterSize, "features."))
-    return(partition(fullData, threshold = threshold))
+  if(ncol(full_data) < cluster_size) {
+    message(paste0("Using regular Partition function since there are < ", cluster_size, "features."))
+    return(partition(full_data, threshold = threshold))
   }
 
   # iteration counters
   i = j = k = l = 0
 
   # Function to go from cluster-specific column numbers to full data column numbers
-  ## fullData  - sample by probe expression data for all probes
-  ## smallData - sample by probe expression data for probes in specific cluster
+  ## full_data  - sample by probe expression data for all probes
+  ## small_data - sample by probe expression data for probes in specific cluster
   ## modules   - indices from partition mapping key
   ## return - indices from partition mapping key for full data
-  fullDataColNumbers <- function(fullData, smallData, modules) {
+  full_data_col_numbers <- function(full_data, small_data, modules) {
 
     # vector of lists to return
-    returnMods <- numeric(length(modules))
+    return_mods <- numeric(length(modules))
 
     for (i in 1:length(modules)) {
       # create vector for full data indices
-      colFull <- numeric(length(unlist(modules[i])))
+      col_full <- numeric(length(unlist(modules[i])))
 
       # get column names
-      colSmall <- colnames(smallData)[unlist(modules[i])]
+      col_small <- colnames(small_data)[unlist(modules[i])]
 
-      # get column indices in full data; append to vector
-      for (j in 1:length(colSmall)) {
-        colFull[j] <- grep(colSmall[j], colnames(fullData))
+      # get column indices in full data
+      for (j in 1:length(col_small)) {
+        col_full[j] <- grep(col_small[j], colnames(full_data))
       }
 
       # add list of indices to return vector
-      returnMods[i] <- list(colFull)
+      return_mods[i] <- list(col_full)
     }
-
-    return(returnMods)
   }
 
   # tracking variables
-  masterCluster <- data.frame(colName = colnames(fullData), cluster = 1)
-  numModules    <- 0
+  master_cluster <- data.frame(col_name = colnames(full_data), cluster = 1)
+  num_modules    <- 0
 
   # use genie (fast agglomerative hierarchical clustering) to cluster data into size 3,000 chunks
   ## transpose data since genie clusters on rows
-  clust                 <- genie(t(fullData), k = ceiling(ncol(fullData)/3000), gini_threshold = 0.05)
-  masterCluster$cluster <- clust
-  clustSize             <- table(clust)
+  clust                 <- genie(t(full_data), k = ceiling(ncol(full_data)/3000), gini_threshold = 0.05)
+  master_cluster$cluster <- clust
+  clust_size             <- table(clust)
 
   # Function to continually reduce clusters until largest cluster has less than 4,000 features
-  # cs - clustSize, numeric vector of cluster sizes
-  # fd - fullData, sample x feature
-  reduceClust <- function(cs, fd) {
+  # cs - clust_size, numeric vector of cluster sizes
+  # fd - full_data, sample x feature
+  reduce_clust <- function(cs, fd) {
     # bookkeeping
-    largestClust <- max(masterCluster$cluster)
-    unique_vals  <- unique(masterCluster$clust)
-    clusters     <- masterCluster$cluster
+    largest_clust <- max(master_cluster$cluster)
+    unique_vals  <- unique(master_cluster$clust)
+    clusters     <- master_cluster$cluster
 
     # keep looping until all clusters are under max cluster size
-    while(max(cs) > clusterSize) {
+    while(max(cs) > cluster_size) {
       for (l in 1:length(cs)) {
         # if cluster size is over max cluster size...
-        if(cs[l] > clusterSize) {
+        if(cs[l] > cluster_size) {
           # cluster again
-          newClust <- genie(t(fd[, which(clusters == unique_vals[l])]),
-                            k = ceiling(cs[l]/clusterSize),
+          new_clust <- genie(t(fd[, which(clusters == unique_vals[l])]),
+                            k = ceiling(cs[l]/cluster_size),
                             gini_threshold = 0.05)
 
           # if genie doesn't do anything, use kmeans
-          if(length(unique(newClust)) == 1 | min(table(newClust)) < 50) {
-            newClust <- kmeans(t(fd[, which(clusters == unique_vals[l])]),
-                               centers = ceiling(cs[l]/clusterSize))$cluster
+          if(length(unique(new_clust)) == 1 | min(table(new_clust)) < 50) {
+            new_clust <- kmeans(t(fd[, which(clusters == unique_vals[l])]),
+                               centers = ceiling(cs[l]/cluster_size))$cluster
           }
 
           # reassign cluster numbers; use <<- to rewrite data from dataframe defined outside function
-          masterCluster$cluster[which(clusters == unique_vals[l])] <<- newClust + largestClust
+          master_cluster$cluster[which(clusters == unique_vals[l])] <<- new_clust + largest_clust
 
           # update largest cluster number
-          largestClust <- max(masterCluster$cluster)
+          largest_clust <- max(master_cluster$cluster)
         }
       }
 
       # update bookkeeping variables
-      cs           <- table(masterCluster$cluster)
-      unique_vals  <- sort(unique(masterCluster$clust))
-      clusters     <- masterCluster$cluster
+      cs           <- table(master_cluster$cluster)
+      unique_vals  <- sort(unique(master_cluster$clust))
+      clusters     <- master_cluster$cluster
     }
   }
 
-  reduceClust(cs = clustSize, fd = fullData)
-  message(paste0(length(unique(masterCluster$cluster)), " super clusters identified. Beginning Partition."))
-  message(paste0("Maximum cluster size: ", max(table(masterCluster$cluster))))
+  reduce_clust(cs = clust_size, fd = full_data)
+  message(paste0(length(unique(master_cluster$cluster)), " super clusters identified. Beginning Partition."))
+  message(paste0("Maximum cluster size: ", max(table(master_cluster$cluster))))
 
   # set up progress bar
-  n_iter <- length(unique(masterCluster$cluster))
+  n_iter <- length(unique(master_cluster$cluster))
   pb     <- progress_bar$new(format =
                                "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
                              total = n_iter,
@@ -153,20 +151,20 @@ superPartition <- function(fullData, threshold = 0.5, clusterSize = 4000, partit
 
   ## first cluster
   # get initial partition to build off
-  part_master <- partition(fullData[, which(masterCluster$cluster == unique(masterCluster$cluster)[1])],
+  part_master <- partition(full_data[, which(master_cluster$cluster == unique(master_cluster$cluster)[1])],
                            threshold, partitioner, tolerance, niter, x, .sep)
 
   # update indices for each module
-  modRows <- grep("reduced_var_", part_master$mapping_key$variable)
-  suppressWarnings(part_master$mapping_key$indices <- fullDataColNumbers(fullData = fullData,
-                                                                         smallData = fullData[, which(masterCluster$cluster == unique(masterCluster$cluster)[1])],
+  mod_rows <- grep("reduced_var_", part_master$mapping_key$variable)
+  suppressWarnings(part_master$mapping_key$indices <- full_data_col_numbers(full_data = full_data,
+                                                                         small_data = full_data[, which(master_cluster$cluster == unique(master_cluster$cluster)[1])],
                                                                          modules = part_master$mapping_key$indices))
 
   # update number of modules
-  numModules  <- numModules + length(grep("reduced_var_", part_master$mapping_key$variable))
+  num_modules  <- num_modules + length(grep("reduced_var_", part_master$mapping_key$variable))
 
-  # create superPartition column in mapping key
-  part_master$mapping_key$superPartition <- 1
+  # create super_partition column in mapping key
+  part_master$mapping_key$super_partition <- 1
 
   # progress bar updates
   pb$tick()
@@ -175,15 +173,15 @@ superPartition <- function(fullData, threshold = 0.5, clusterSize = 4000, partit
   for (i in 2:n_iter) {
 
     # what to do if cluster is of size one
-    if(sum(masterCluster$cluster == unique(masterCluster$cluster)[i]) == 1) {
+    if(sum(master_cluster$cluster == unique(master_cluster$cluster)[i]) == 1) {
       # cbind data to master partition reduced data
       part_master$reduced_data <- cbind(part_master$reduced_data,
-                                        fullData[, which(masterCluster$cluster == unique(masterCluster$cluster)[i])])
+                                        full_data[, which(master_cluster$cluster == unique(master_cluster$cluster)[i])])
 
-      # rbind data to master partition mapping key
-      name <- masterCluster$colName[which(masterCluster$cluster == unique(masterCluster$cluster)[i])]
+      # add data to master partition mapping key
+      name <- master_cluster$col_name[which(master_cluster$cluster == unique(master_cluster$cluster)[i])]
       part_master$mapping_key <- rbind(part_master$mapping_key,
-                                       c(name, name, 1, grep(name, colnames(fullData))))
+                                       c(name, name, 1, grep(name, colnames(full_data))))
 
       # end iteration
       pb$tick()
@@ -191,46 +189,47 @@ superPartition <- function(fullData, threshold = 0.5, clusterSize = 4000, partit
     }
 
     ## partition
-    part_clust <- partition(fullData[, which(masterCluster$cluster == unique(masterCluster$cluster)[i])],
+    part_clust <- partition(full_data[, which(master_cluster$cluster == unique(master_cluster$cluster)[i])],
                             threshold, partitioner, tolerance, niter, x, .sep)
 
     ## reduced data
     # get column indices of reduced vars
-    modCols <- grep("reduced_var_", colnames(part_clust$reduced_data))
+    mod_cols <- grep("reduced_var_", colnames(part_clust$reduced_data))
 
     # update module numbers
-    for (j in 1:length(modCols)) {
-      colnames(part_clust$reduced_data)[modCols[j]] <- paste0("reduced_var_", numModules+j)
+    for (j in 1:length(mod_cols)) {
+      colnames(part_clust$reduced_data)[mod_cols[j]] <- paste0("reduced_var_", num_modules+j)
     }
 
-    # cbind to master partition
+    # add to master partition
     part_master$reduced_data <- cbind(part_master$reduced_data, part_clust$reduced_data)
 
     ## mapping key
     # get row indices of reduced vars
-    modRows <- grep("reduced_var_", part_clust$mapping_key$variable)
+    mod_rows <- grep("reduced_var_", part_clust$mapping_key$variable)
 
     # update module number
-    for (k in 1:length(modRows)) {
-      part_clust$mapping_key$variable[modRows[k]] <- paste0("reduced_var_", numModules+k)
+    for (k in 1:length(mod_rows)) {
+      part_clust$mapping_key$variable[mod_rows[k]] <- paste0("reduced_var_", num_modules+k)
     }
 
     # update indices
-    suppressWarnings(part_clust$mapping_key$indices <- fullDataColNumbers(fullData = fullData,
-                                                                          smallData = fullData[, which(masterCluster$cluster == unique(masterCluster$cluster)[i])],
+    suppressWarnings(part_clust$mapping_key$indices <- full_data_col_numbers(full_data = full_data,
+                                                                          small_data = full_data[, which(master_cluster$cluster == unique(master_cluster$cluster)[i])],
                                                                           modules = part_clust$mapping_key$indices))
 
-    # add superPartition column
-    part_clust$mapping_key$superPartition <- i
+    # add super_partition column
+    part_clust$mapping_key$super_partition <- i
 
-    # rbind to master partition
+    # add to master partition
     part_master$mapping_key <- rbind(part_master$mapping_key, part_clust$mapping_key)
 
     # add new modules to overall count
-    numModules <- numModules + length(modCols)
+    num_modules <- num_modules + length(mod_cols)
 
     # progress bar updates
     pb$tick()
   }
-  return(part_master)
+
+  part_master <- part_master
 }
